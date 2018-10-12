12ea48382a5cba218e18c4e8e6624ea84e5ffe41
ehacker98 <36097426+ehacker98@users.noreply.github.com>
2018-10-12 15:38:19 -0400

    Update planner.java

--- planner.java
+++ planner.java
@@ -1,9 +1,29 @@
 package assignment2SourceCode;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+
 public class planner extends agent{
-  public void createservice(){
-  }
-  
-  public void deleteservice(){
-  }
+    Scanner scan = new Scanner(System.in);
+    String x;
+    String y;
+    String z;
+
+    public void createservice(){
+        System.out.println("What is the service number?");
+        x = scan.nextLine();
+        System.out.println("What is the service date?");
+        y = scan.nextLine();
+        System.out.println("What is the service name?");
+        z = scan.nextLine();
+        writer.println("CAS " + x + " 0 " + " 00000 " + z + " " + y + "\n");
+    }
+
+    public void deleteservice(){
+    }
+
+    public planner() throws FileNotFoundException, UnsupportedEncodingException {
+    }
 }
