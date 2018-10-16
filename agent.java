286a0f5fa0d6c7ab390c02ceb194a3aef292fc95
p-girard <44072055+p-girard@users.noreply.github.com>
2018-10-16 16:27:07 -0400

    Update agent.java

--- agent.java
+++ agent.java
@@ -11,20 +11,16 @@ public class agent {
     String z;
     //serviceCancels = [0] create array of 0s, length of array is number of services in valid services file
 
-    public String sellticket(){
-        //Need to check for valid service number and number of tickets
+    public String sellticket(){ //if sellticket called, ask for service and number of tickets to sell, then push results to output file
         System.out.println("For what service would you like to sell tickets?");
         x = scan.nextLine();
         System.out.println("How many tickets would you like to sell?");
         y = scan.nextLine();
-        //if x in valid service file
-        //if y <= 1000 && y >=1
+
         return ("SEL " + x + " " + y + " 00000 " + " **** " + " 0 \n");
     }
 
-    public String cancelticket(){
-        //Need to check that no more than 20 tickets are being cancelled for specific service within this session
-        //Need to check tickets are being cancelled for a valid service
+    public String cancelticket(){ //if cancelticket called, ask for service and number of tickets to cancel, then push results to output file
         System.out.println("For what service would you like to cancel tickets?");
         x = scan.nextLine();
         System.out.println("How many tickets would you like to cancel?");
@@ -32,7 +28,7 @@ public class agent {
         return ("CAN " + x + " " + y + " 00000 " + " **** " + " 0 \n");
     }
 
-    public String changeticket(){
+    public String changeticket(){ //if changeticket called, ask for service and number of tickets to change, and service number to change to, then push results to output file
         System.out.println("From which service would you like to change tickets?");
         x = scan.nextLine();
         System.out.println("How many tickets would you like?");
